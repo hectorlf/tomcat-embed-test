69d5ff3508f4a4082d65f13b39e85c2657387477
Héctor <hectorlf@users.noreply.github.com>
2015-02-05 13:48:45 +0100

    Bare tomcat init code

--- src/main/java/test/Bootstrap.java
+++ src/main/java/test/Bootstrap.java
@@ -1,9 +1,30 @@
 package test;
 
+import java.io.File;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
+
 public class Bootstrap {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Starting Tomcat");
+		Tomcat t = new Tomcat();
+		t.setPort(8123);
+		try {
+			File base = new File(System.getProperty("java.io.tmpdir"));
+			System.out.println("Rott context basedir: " + base.getAbsolutePath());
+			Context rootCtx = t.addContext("", base.getAbsolutePath());
+			System.out.println("Tomcat start()");
+			t.start();
+			System.out.println("Tomcat started");
+			//t.getServer().await();
+			System.out.println("Stopping Tomcat");
+			t.stop();
+			System.out.println("Tomcat stopped");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
